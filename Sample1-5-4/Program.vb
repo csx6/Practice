12bf07568d12a181e776369386b5196ea5303b6f
vb_uc22090@outlook.jp <vb_uc22090@outlook.jp>
2022-04-22 12:56:01 +0900

    12:55

--- Sample1-5-4/Program.vb
+++ Sample1-5-4/Program.vb
@@ -2,6 +2,26 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Dim a, b, intSum, tmp As Integer
+        Console.WriteLine("aの値")
+        a = Console.ReadLine()
+        Console.WriteLine("bの値")
+        b = Console.ReadLine()
+
+        If a > b Then 'a,bの数値の並び替えのため、変数tmpを使用する
+            tmp = a
+            a = b
+            b = tmp
+        End If
+
+        Console.WriteLine("結果")
+        For i As Integer = a To b
+            intSum += i
+        Next
+        Console.WriteLine(a & "から" & b & "までの合計値は" & intSum & "です。")
+        Console.Read()
+
+
+
     End Sub
 End Module
