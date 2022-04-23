8a8331a16ba020355e469e79c422c172eda14380
Administrator <Administrator@SKY-20170617ADF>
2022-04-23 09:25:27 +0900

    star

---
+++ Practice_star/Program.vb
@@ -0,0 +1,84 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+
+        ''1.
+        'Console.WriteLine("0.5 * 5 三角アスタリスク")
+        'For i As Integer = 1 To 5
+        '    For j As Integer = 1 To 5
+        '        Console.Write("*")
+        '    Next
+        '    Console.WriteLine()
+        'Next
+
+        '2. 
+        Console.WriteLine("左上 三角アスタリスク")
+        For i As Integer = 1 To 5
+            For j As Integer = 1 To 5
+                If j >= i Then
+                    Console.Write("*")
+                    'Else
+                    '    Console.Write(" ")
+                End If
+            Next
+            Console.WriteLine()
+        Next
+
+        '2. 
+        Console.WriteLine("左上 三角アスタリスク")
+        For i As Integer = 1 To 5
+            For j As Integer = 1 To 5
+                If j <= (6 - i) Then
+                    Console.Write("*")
+                Else
+                    Console.Write(" ")
+                End If
+            Next
+            Console.WriteLine()
+        Next
+
+        '3. 
+        Console.WriteLine("右上 三角アスタリスク")
+        For i As Integer = 1 To 5
+            For j As Integer = 1 To 5
+                If j >= i Then
+                    Console.Write("*")
+                Else
+                    Console.Write(" ")
+                End If
+            Next
+            Console.WriteLine()
+        Next
+
+        '4. 
+        Console.WriteLine("左下 三角アスタリスク")
+        For i As Integer = 1 To 5
+            For j As Integer = 1 To 5
+                If i >= j Then
+                    Console.Write("*")
+                Else
+                    Console.Write(" ")
+                End If
+            Next
+            Console.WriteLine()
+        Next
+
+        '5. 
+        Console.WriteLine("右下 三角アスタリスク")
+        For i As Integer = 1 To 5
+            For j As Integer = 1 To 5
+                If (6 - i) <= j Then
+                    Console.Write("*")
+                Else
+                    Console.Write(" ")
+                End If
+            Next
+            Console.WriteLine()
+        Next
+
+
+
+        Console.Read()
+    End Sub
+End Module
